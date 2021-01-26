497faf096c050eaae98c227ed1435ffdfacf976c
Jonathan Ross <jross@taeltech.com>
2021-01-26 14:37:41 +0800

    add header shadow on scroll, video area styling

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
   'use strict';
 
-  var window_width = $(window).width(),
-    window_height = window.innerHeight,
+  const window_height = window.innerHeight,
     header_height = $('.default-header').height(),
-    header_height_static = $('.site-header.static').outerHeight(),
     fitScreen = window_height - header_height;
 
   $('.fullscreen').css('height', window_height);
